39d3fb259e64b9db230f75cc1f82cb44b300637e
Javier Franco <javier.franco18@gmail.com>
2019-11-06 15:30:10 +0100

    Solution Yelp - Missing styles

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :admin do
+    resources :restaurants, only: [:update, :destroy] do
+      resources :reviews, only: [:update, :destroy]
+    end
+  end
+  resources :restaurants, except: [:update, :destroy] do
+    resources :reviews, only: [:new, :create]
+  end
+  resources :reviews, only: :show
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,79 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+Review.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Buffalo Wings',
+    address:      'Kadath E2 7JE',
+    phone_number: '345 234234',
+    category:     'belgian'
+  },
+  {
+    name:         'Dallas BBQ',
+    address:      'Innsmouth 56A St',
+    phone_number: '356 5252355',
+    category:     'french'
+  },
+  {
+    name:         'Rock n Sushi',
+    address:      'Miskatonic E1 6PQ',
+    phone_number: '345 55234345',
+    category:     'japanese'
+  },
+  {
+    name:         'Tommy Mels',
+    address:      'Leng St 6PQ',
+    phone_number: '331 236654',
+    category:     'italian'
+  },
+  {
+    name:         'Poke go',
+    address:      'Arkham E1',
+    phone_number: '842 9873204',
+    category:     'italian'
+  }
+]
+
+puts 'Creating reviews...'
+reviews_attributes = [
+  {
+    restaurant_id: 1,
+    content: "Not understanding that you're a horrible person doesn't make you less of a horrible person",
+    rating: 3
+  },
+  {
+    restaurant_id: 1,
+    content: "Spaghetti or not, here I come",
+    rating: 4
+  },
+  {
+    restaurant_id: 2,
+    content: "I need to go take a shower so I can't tell if I'm crying or not",
+    rating: 2
+  },
+  {
+    restaurant_id: 3,
+    content: "Ow, crap. I hate this. Running is terrible. Everything is the worst",
+    rating: 3
+  },
+  {
+    restaurant_id: 4,
+    content: "For a lot of people, life is just one long, hard kick in the urethra",
+    rating: 5
+  },
+  {
+    restaurant_id: 5,
+    content: "Yes, I ate all the muffins, because I have no self-control and I hate myself",
+    rating: 4
+  },
+  {
+    restaurant_id: 5,
+    content: "That's the problem with life, either you know what you want and you don't get what you want, or you get what you want and then you don't know what you want",
+    rating: 2
+  }
+]
+Restaurant.create!(restaurants_attributes)
+Review.create!(reviews_attributes)
+puts 'Finished!'
